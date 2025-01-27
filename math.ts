4d85c4a5338691d9c943cea108de16effb696264
Kanad <codewizardkanad@gmail.com>
2025-01-27 17:17:58 +0530

    Added math.ts file

---
+++ math.ts
@@ -0,0 +1,9 @@
+function addNums(a: number, b: number) {
+  console.log(`The sum of ${a} and ${b} is: ${a + b}`);
+}
+function substractNums(a: number, b: number) {
+  console.log(`The substraction of ${a} and ${b} is: ${a - b}`);
+}
+function multiplyNums(a: number, b: number) {
+  console.log(`The multiplication of ${a} and ${b} is: ${a * b}`);
+}
