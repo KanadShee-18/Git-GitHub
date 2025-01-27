df0009e79782c7b5dc1787c1765ccf534246a8bc
Kanad <codewizardkanad@gmail.com>
2025-01-27 16:31:09 +0530

    Revert back index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ const myName1: string = "Kanad";
 function sayName() {
   console.log(`My name is: `, myName1);
 }
-
+function isAbove18(age: number) {
+  return age >= 18;
+}
 function newFn() {
   console.log("Hey! I am new function.");
 }
